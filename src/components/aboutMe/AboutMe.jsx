fd091c538902c3bbea671b9d06861dc3aedb9ffd
Taras Kunyk <kunykty@gmail.com>
2020-05-29 18:50:07 +0300

    fixes contact container

--- src/components/aboutMe/AboutMe.jsx
+++ src/components/aboutMe/AboutMe.jsx
@@ -3,57 +3,57 @@ import './AboutMe.scss';
 import Input from "../input/Input";
 
 
-const contacts = [
-    {
-        id: 1,
-        fullName: 'Velazques',
-        messagesFrom: [{
-            date: '19/05/2020',
-            time: '12:21:51',
-            value: 'Hi, Taras!'
-        }],
-        messagesTo: [{
-            date: '19/05/2020',
-            time: '09:54:51',
-            value: 'Hi, Teo!'
-        }]
-    },
-    {
-        id:2,
-        fullName: 'Barrera',
-        messagesFrom: [{
-            date: '25/04/2020',
-            time: '22:27:51',
-            value: 'Hi, Taras!'
-        }],
-        messagesTo: [{
-            date: '26/04/2020',
-            time: '19:31:51',
-            value: 'Hi, Teo!'
-        }]
-    },
-    {
-        id:3,
-        fullName: 'Alice Freeman',
-        messagesFrom: [{
-            date: '21/05/2020',
-            time: '14:25:51',
-            value: 'Hi, Taras!'
-        }],
-        messagesTo: [{
-            date: '26/04/2020',
-            time: '19:46:51',
-            value: 'Hi, Teo!'
-        }]
-    }
-];
+// const contacts = [
+//     {
+//         id: 1,
+//         fullName: 'Velazques',
+//         messages: [{
+//             date: '19/05/2020',
+//             time: '12:21:51',
+//             value: 'Hi, Taras!'
+//         }],
+//         {
+//             date: '19/05/2020',
+//             time: '09:54:51',
+//             value: 'Hi, Teo!'
+//         }]
+//     },
+//     {
+//         id:2,
+//         fullName: 'Barrera',
+//         messagesFrom: [{
+//             date: '25/04/2020',
+//             time: '22:27:51',
+//             value: 'Hi, Taras!'
+//         }],
+//         messagesTo: [{
+//             date: '26/04/2020',
+//             time: '19:31:51',
+//             value: 'Hi, Teo!'
+//         }]
+//     },
+//     {
+//         id:3,
+//         fullName: 'Alice Freeman',
+//         messagesFrom: [{
+//             date: '21/05/2020',
+//             time: '14:25:51',
+//             value: 'Hi, Taras!'
+//         }],
+//         messagesTo: [{
+//             date: '26/04/2020',
+//             time: '19:46:51',
+//             value: 'Hi, Teo!'
+//         }]
+//     }
+// ];
 
 
 class AboutMe extends Component {
 
-    componentDidMount() {
-        contacts.map(contact => this.props.setContactName(contact))
-    };
+    // componentDidMount() {
+    //     contacts.map(contact => this.props.setContactName(contact))
+    // };
 
     render() {
         return (
--- src/reducer.js
+++ src/reducer.js
@@ -1,24 +1,100 @@
-const initialState = {
+const initialObjects = {
     fullName: 'Taras Kunyk',
     contacts: [
         {
-            id:0,
+            id: 0,
             fullName: 'Josephina',
-            messagesFrom: [{
-                date: '27/05/2020',
-                time: '22:27:51',
-                value: 'Hi, Taras!'
-            }],
-            messagesTo: [{
-                date: '27/05/2020',
-                time: '22:28:51',
-                value: 'Hi, Josephina!'
-            }]
+            messages: [
+                {
+                    id: 1,
+                    date: '27/05/2020',
+                    time: '22:27:51',
+                    value: 'Hi, Taras!'
+                },
+                {
+                    id: 2,
+                    date: '27/05/2020',
+                    time: '22:28:51',
+                    value: 'Hi, Josephina!'
+                }
+            ]
         },
+        {
+            id: 1,
+            fullName: 'Donald Trump',
+            messages: [
+                {
+                    id: 1,
+                    date: '27/05/2020',
+                    time: '22:27:51',
+                    value: 'How you doing?!'
+                },
+                {
+                    id: 2,
+                    date: '27/05/2020',
+                    time: '22:28:51',
+                    value: 'Good!'
+                }
+            ]
+        },
+        {
+            id: 2,
+            fullName: 'Elon Musk',
+            messages: [
+                {
+                    id: 1,
+                    date: '27/05/2020',
+                    time: '22:27:51',
+                    value: 'Do you want new Tesla?'
+                },
+                {
+                    id: 2,
+                    date: '27/05/2020',
+                    time: '22:28:51',
+                    value: 'yeah, why not'
+                }
+            ]
+        },
+        {
+            id: 3,
+            fullName: 'Michael',
+            messages: [
+                {
+                    id: 1,
+                    date: '27/05/2020',
+                    time: '22:27:51',
+                    value: 'how are you?'
+                },
+                {
+                    id: 2,
+                    date: '27/05/2020',
+                    time: '22:28:51',
+                    value: 'perfect'
+                }
+            ]
+        },
+        {
+            id: 4,
+            fullName: 'Bryan',
+            messages: [
+                {
+                    id: 1,
+                    date: '27/05/2020',
+                    time: '22:27:51',
+                    value: 'I am so tired'
+                },
+                {
+                    id: 2,
+                    date: '27/05/2020',
+                    time: '22:28:51',
+                    value: 'okay'
+                }
+            ]
+        }],
+        currentContact:''
+}
 
-    ],
-    currentContact: ''
-};
+const initialState = initialObjects;
 
 
 export const reducer = (state = initialState, action) => {
@@ -38,3 +114,6 @@ export const reducer = (state = initialState, action) => {
             return state;
     }
 };
+
+
+
